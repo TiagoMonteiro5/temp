82a4c3d7252129a46e26cfc6e40356e8099f346e
Teixas93 <72864174+Teixas93@users.noreply.github.com>
2021-03-17 00:18:46 +0000

    Add files via upload

--- Game_Categories/game_categories.py
+++ Game_Categories/game_categories.py
@@ -1,11 +1,16 @@
 from flask import Flask 
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
-
+api = Api(app)
 @app.route("/")
 @app.route("/Categories")
-def home():
-    return "<h1>Categories Page</h1>"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+class Game_Categories(Resource):
+    def get(self):
+        return "This is the game categories"
+
+api.add_resource(Game_Categories, '/')
+
+if __name__ == '__main__':     
+	app.run(debug=True, host='0.0.0.0')
